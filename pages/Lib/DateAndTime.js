ed152f302fa4d4a8425ce9f6255f4d43cc1c3542
rmstcvw <rmstcvw@gmail.com>
2026-05-19 14:57:01 +0300

    fix DateAndTime

--- pages/Lib/DateAndTime.js
+++ pages/Lib/DateAndTime.js
@@ -1,7 +1,48 @@
 import globalVars from "../../globalVars.js";
 
 let data_descr = `${globalVars.titleTextTagOpen}
-Дата и время
+Замерить длительность:
+${globalVars.titleTextTagClose}
+${globalVars.textTagOpen}
+Время начала в мс (вопрос t1):
+${globalVars.textTagClose}
+${globalVars.codeTagOpen}
+if (Q.isAnswered) return testAnswered(Q);
+Q.openValueInt = Date.now();    
+return testAnswered(Q);
+${globalVars.codeTagClose}
+${globalVars.textTagOpen}
+Время окончания в (вопрос t2):
+${globalVars.textTagClose}
+${globalVars.codeTagOpen}
+if (Q.isAnswered) return testAnswered(Q);
+Q.openValueInt = Date.now();    
+return testAnswered(Q);
+${globalVars.codeTagClose}
+${globalVars.textTagOpen}
+Длительность в (вопрос t3):
+${globalVars.textTagClose}
+${globalVars.codeTagOpen}
+if (Q.isAnswered) return testAnswered(Q);
+let duration = t2.answer.openValueInt - t1.answer.openValueInt;
+Q.openValueInt = duration;
+return testAnswered(Q);
+${globalVars.codeTagClose}
+${globalVars.textTagOpen}
+Длительность _м_с (вопрос t4):
+${globalVars.textTagClose}
+${globalVars.codeTagOpen}
+if (Q.isAnswered) return testAnswered(Q);
+
+let duration = t2.answer.openValueInt - t1.answer.openValueInt;
+let seconds = Math.floor(duration / 1000);
+
+Q.openValueTxt = (\`Длительность: \${Math.floor(seconds / 60)} мин \${seconds % 60} сек\`);
+
+return testAnswered(Q);
+${globalVars.codeTagClose}
+${globalVars.titleTextTagOpen}
+Как использовать тип вопроса Дата и Время:
 ${globalVars.titleTextTagClose}
 ${globalVars.textTagOpen}
 Позволяет ввести или выбрать в календаре время или дату, или все вместе.
